f09cd04e216b33fa0b78f9aee1cffad488e10569
AntonPav <streetreadger@gmail.com>
2023-01-10 16:56:07 +0200

    Create page, with uploading, and downloading info

--- src/main/java/com/ednipro/dniprotesttask/controller/FileUploadController.java
+++ src/main/java/com/ednipro/dniprotesttask/controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.ednipro.dniprotesttask.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,13 +17,19 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class FileUploadController {
@@ -35,6 +42,14 @@ public class FileUploadController {
     @PostConstruct
     private void init() {
         storageService.init();
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public ModelAndView listUploadedFiles() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("uploadForm");
+        return modelAndView;
     }
 
     @PostMapping("/upload")
